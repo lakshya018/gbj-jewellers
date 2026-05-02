db3ff4ebac945f6815f613d9a8c283f95ea2212d
lakshya018 <lakshyasoni018@gmail.com>
2026-05-02 18:34:42 +0530

    large files upload

--- payload.config.ts
+++ payload.config.ts
@@ -54,7 +54,10 @@ export default buildConfig({
   plugins: [
     vercelBlobStorage({
       collections: {
-        media: true,
+        media: {
+          disableLocalStorage: true,
+          clientUploads: true,
+        },
       },
       token: process.env.BLOB_READ_WRITE_TOKEN,
     }),
